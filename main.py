2ac65da648e54cbf0b34d33225fc6d3ab04290af
Lucian Patrasc <lpatrasc@gmail.com>
2024-11-29 09:55:01 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,20 +7,10 @@ screen.setup(800, 600)
 screen.title("PONG")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-def go_up():
-    new_y = paddle.ycor() +20
-    paddle.goto(paddle.xcor(), new_y)
 
-def go_down():
-    new_y = paddle.ycor() -20
-    paddle.goto(paddle.xcor(), new_y)
 
 screen.listen()
 screen.onkey(go_up, "Up")
