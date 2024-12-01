62b884dedb9e919332602a9fc99fdfe6cb8fe262
Lucian Patrasc <lpatrasc@gmail.com>
2024-12-01 13:33:49 +0200

    10th commit - left scoreboard and right scoreboard are ON

---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+from unicodedata import normalize
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.updete_scoreboard()
+
+    def updete_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.updete_scoreboard()
+
